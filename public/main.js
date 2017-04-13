3497b27541a51340709af00ac5581fd024d723e0
Robert-Jung <rjinjung@gmail.com>
2017-04-13 11:26:48 -0700

    Added conditional statement to remove old blob #1

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-function createAudioElement (blobURL) {
+function createAudioElement(blobURL) {
   var audio = document.createElement('audio')
   audio.setAttribute('src', blobURL)
   audio.setAttribute('controls', 'controls')
@@ -7,25 +7,29 @@ function createAudioElement (blobURL) {
 }
 
 navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-    var chunks = [];
-    var recorder = new MediaRecorder(stream)
+  var chunks = [];
+  var recorder = new MediaRecorder(stream)
+  let blobURL
 
-    recorder.ondataavailable = e => {
-      chunks.push(e.data)
+  recorder.ondataavailable = e => {
+    chunks.push(e.data)
 
-      if (recorder.state == 'inactive') {
-          const blob = new Blob(chunks, { type: 'audio/wav' })
-          const blobURL = URL.createObjectURL(blob)
-          const audioElement = createAudioElement(blobURL)
-          document.querySelector('#audioPlayback').appendChild(audioElement)
+    if (recorder.state === 'inactive') {
+      if (blobURL) {
+        URL.revokeObjectURL(blobURL)
       }
+      const blob = new Blob(chunks, { type: 'audio/wav' })
+      blobURL = URL.createObjectURL(blob)
+      const audioElement = createAudioElement(blobURL)
+      document.querySelector('#audioPlayback').appendChild(audioElement)
     }
+  }
 
-    document.getElementById('record').addEventListener('click', () => {
-      recorder.start()
-    })
-    document.getElementById('stopRecord').addEventListener('click', () => {
-      recorder.stop()
-    })
+  document.getElementById('record').addEventListener('click', () => {
+    recorder.start()
+  })
+  document.getElementById('stopRecord').addEventListener('click', () => {
+    recorder.stop()
+  })
 
-  }).catch(console.error)
+}).catch(console.error)
