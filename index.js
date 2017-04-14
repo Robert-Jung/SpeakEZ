70d7348f33a8fa0e626cd8ec0d12accd78452f41
Robert-Jung <rjinjung@gmail.com>
2017-04-14 13:31:01 -0700

    Finally converts audio to wav #3

--- index.js
+++ index.js
@@ -4,41 +4,25 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var multer = require('multer')
 var ffmpeg = require('fluent-ffmpeg')
-var autoReap = require('multer-autoreap')
+var convertToWav = require('./convert-to-wav')
 
 var app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-//upload audio blob
-var upload = multer({ storage: storage })
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './tmp/')
-  },
-  filename: function (req, file, cb) {
-      cb(null,  file.originalname );
-  }
-});
+//upload audio blob and convert to audio wav
+var upload = multer({ dest:'tmp/' })
 
 app.post('/tmp', upload.single('file'), (req,res) => {
   console.log(req.file)
-  res.sendStatus(200)
+  convertToWav(req.file.path)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(() => {
+      res.sendStatus(500)
+  })
 })
-
-//convert audio
-var audio = '/tmp/audio'
-
-ffmpeg(audio)
-.toFormat('wav')
-.on('error', () => {
-  console.log('An error has occured ')
-})
-.on('end', () => {
-  console.log('Finished!')
-})
-.save('./transcribe/audio.wav')
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
