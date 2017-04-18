9fbae1341466aa8fdd28c54508a39def213da88d
Robert-Jung <rjinjung@gmail.com>
2017-04-18 13:01:44 -0700

    User can see all inventory #7

--- index.js
+++ index.js
@@ -6,13 +6,34 @@ var convertToWav = require('./convert-to-wav')
 var deleteWav = require('./delete-wav')
 var callWatson = require('./call-watson')
 
+var products = [
+  {
+    upc: '1234567890123',
+    name: 'hot wheels',
+    inventory: 12,
+    price: 13.99
+  },
+  {
+    upc: '7089574398512',
+    name: 'doll',
+    inventory: 23,
+    price: 1.99
+  },
+  {
+    upc: '834534845701',
+    name: 'catan',
+    inventory: 50,
+    price: 48.71
+  }
+]
+
 var app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
 var upload = multer({ dest:'tmp/' })
 
-app.post('/command', upload.single('file'), (req,res) => {
+app.post('/command', upload.single('file'), (req, res) => {
   console.log(req.file)
   convertToWav(req.file.path)
     .then((fileName) => {
@@ -25,6 +46,15 @@ app.post('/command', upload.single('file'), (req,res) => {
     .catch( () => {
       res.sendStatus(500)
   })
+})
+
+app.post('/product', (req, res) => {
+  var product = req.body
+    products.push(product)
+})
+
+app.get('/product', (req, res) => {
+  res.json(products)
 })
 
 app.listen(3000, () => {
--- public/main.js
+++ public/main.js
@@ -43,3 +43,49 @@ function uploadFile(blob) {
     console.log('Request Failed')
   })
 }
+
+
+
+function fetchProduct() {
+  var fetchPromise = fetch('/product')
+  var productPromise = fetchPromise.then(res => {
+    return res.json()
+  })
+  return productPromise
+}
+
+function renderProduct(product) {
+  var $product = document.createElement('row')
+  var $upc = document.createElement('div')
+  var $name = document.createElement('div')
+  var $inventory = document.createElement('div')
+  var $price = document.createElement('div')
+
+  $upc.textContent = product.upc
+  $name.textContent = product.name
+  $inventory.textContent = product.inventory
+  $price.textContent = '$' + product.price
+
+  $upc.setAttribute('class','col-md-2')
+  $name.setAttribute('class', 'col-md-4')
+  $inventory.setAttribute('class','col-md-2')
+  $inventory.setAttribute('class','col-md-2')
+
+  $product.appendChild($upc)
+  $product.appendChild($name)
+  $product.appendChild($inventory)
+  $product.appendChild($price)
+
+  return $product
+}
+
+var $main = document.querySelector('#main')
+
+fetchProduct()
+  .then(products => {
+    return products.map(renderProduct)
+  }).then($products => {
+    $products.forEach($product => {
+      $main.appendChild($product)
+    })
+  })
