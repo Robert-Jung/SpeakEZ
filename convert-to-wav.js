3e5b892827f3003dd0640f494625701b1a5d066c
Robert-Jung <rjinjung@gmail.com>
2017-08-16 09:52:44 -0700

    Adding directory.

--- convert-to-wav.js
+++ convert-to-wav.js
@@ -1,21 +1,27 @@
 //Convert audio blob to wav format.
 
 var ffmpeg = require('fluent-ffmpeg')
+var fs = require('fs')
 
 module.exports = function convertToWav(pathToFile) {
   var fileName = ('audio' + Date.now() + '.wav')
 
   return new Promise((resolve, reject) => {
-    ffmpeg(pathToFile)
-      .toFormat('wav')
-      .on('error', (error) => {
-        console.log('ffmeg failed to save', error)
-        reject()
-      })
-      .on('end', () => {
-        resolve(fileName)
-        console.log('Finished!')
-      })
-    .save('./transcribe/' + fileName)
+    fs.access('./transcribe', fs.constants.W_OK, (err) => {
+      if (err) {
+        fs.mkdirSync('./transcribe')
+      }
+      ffmpeg(pathToFile)
+        .toFormat('wav')
+        .on('error', (error) => {
+          console.log('ffmeg failed to save', error)
+          reject()
+        })
+        .on('end', () => {
+          resolve(fileName)
+          console.log('Finished!')
+        })
+      .save('./transcribe/' + fileName)
+    })
   })
 }
